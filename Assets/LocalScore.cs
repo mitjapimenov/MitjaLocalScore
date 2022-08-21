34e0a3799dbede2db083a67f8eda536b17e6026e
heikkinenlaura <77092247+heikkinenlaura@users.noreply.github.com>
2022-08-21 22:26:46 +0300

    cookie clicker

--- Assets/LocalScore.cs
+++ Assets/LocalScore.cs
@@ -9,23 +9,38 @@ public class LocalScore : MonoBehaviour
     public Text numberOut;
     private string key = "0num";
 
+    // clickers score amount
+    private float score;
 
     public void SaveNumber()
     {
-        int i = int.Parse(numberIn.text);
-        PlayerPrefs.SetInt(key,i);
-    }
+        //int i = int.Parse(score);
+        //PlayerPrefs.SetInt(key,i);
 
+        // saves score for future playtime
+        //PlayerPrefs.SetFloat("score", score);
+    }
+    public void AddScore()
+    {
+        // finds saved score
+        score = PlayerPrefs.GetFloat("score");
+        // adds score everytime you click on it
+        score++;
+        // saves the score on every click
+        PlayerPrefs.SetFloat("score", score);
+        // show number in real time
+        numberOut.text = score.ToString();
+    }
 
     public void ShowNumber()
     {
-        numberOut.text = PlayerPrefs.GetInt(key).ToString();
+        numberOut.text = PlayerPrefs.GetFloat("score").ToString();
     }
 
 
     public void DeleteSave()
     {
         PlayerPrefs.DeleteAll();
-        numberOut.text = PlayerPrefs.GetInt(key).ToString();
+        numberOut.text = PlayerPrefs.GetInt("score").ToString();
     }
 }
